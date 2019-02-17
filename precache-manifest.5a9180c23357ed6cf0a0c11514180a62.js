1937f0e3ba2b24d23ed27c7c5b249ca96009e561
Jonathan Kim <jon@than.kim>
2019-02-17 05:51:09 -0500

    Updates

---
+++ precache-manifest.5a9180c23357ed6cf0a0c11514180a62.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "513de8687975ab044337d7e7c34ab24a",
+    "url": "/static/media/white-piece.513de868.svg"
+  },
+  {
+    "revision": "a7ebdc242ff3ac727024666f01dc62be",
+    "url": "/static/media/board.a7ebdc24.svg"
+  },
+  {
+    "revision": "029d504d3c82ef5a3f6eeeab4472d622",
+    "url": "/static/media/black-piece.029d504d.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "76c9cfac08bf681870f9",
+    "url": "/static/js/main.76c9cfac.chunk.js"
+  },
+  {
+    "revision": "23c35d8ff69c3d3dda46",
+    "url": "/static/js/1.23c35d8f.chunk.js"
+  },
+  {
+    "revision": "76c9cfac08bf681870f9",
+    "url": "/static/css/main.73b839a7.chunk.css"
+  },
+  {
+    "revision": "965c109fa3c0e032fdcc98d41638a85c",
+    "url": "/index.html"
+  }
+];
